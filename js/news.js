a5cf8aa253fc1530bf948d7d6177d60ace0bb095
ajoy45 <ajoypust@gmail.com>
2023-03-03 01:37:16 +0600

    rating dynamic done

--- js/news.js
+++ js/news.js
@@ -1,4 +1,7 @@
-
+let fetchData=[];
+console.log(fetchData)
+let categoryName=''
+// console.log(fetchData)
 const loadCategoryName=()=>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res=>res.json())
@@ -22,7 +25,11 @@ const showCategoryId=(id,name)=>{
     try{
           fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
           .then(res=>res.json())
-          .then(data=>showNews(data.data,name))
+          .then(data=>{
+            fetchData=data.data;
+            categoryName=name;
+            showNews(data.data,name)
+          })
     }
     catch(error){
         console.log(error)
@@ -37,7 +44,7 @@ const showNews=(newses,name)=>{
       newsContainer.innerText=''
      newses.forEach(news=>{
         console.log(news)
-        const {thumbnail_url,image_url,title,details,author}=news;
+        const {_id,rating,image_url,title,details,author,total_view}=news;
         const date=new Date(author.published_date);
         const dateFormate=date.toDateString()
         // console.log()
@@ -56,7 +63,7 @@ const showNews=(newses,name)=>{
                 <p class="card-text">${details.slice(0,200)}...</p>
            </div>
           </div>
-          <div class='ps-3 mb-3'>
+          <div class='ps-3 mb-3 d-flex align-items-center justify-content-around'>
                <div class='d-flex align-items-center'>
                     <img class='rounded-circle'width='50px'height='50px' src="${author.img}" alt="">
                <div class=' ps-2'>
@@ -65,12 +72,81 @@ const showNews=(newses,name)=>{
                </div>
                
                </div>
+               <div class='d-flex '>
+                 <i class='fas fa-eye '></i>
+                 <p class='ps-2'>${total_view?total_view:'No view available'}</p>
+               </div>
+               <div class='d-flex align-items-baseline'>
+                ${generatingRating(rating.number)}
+                <p>${rating.number}</p>
+               </div>
+               <div>
+               <i onclick='loadNewsDetails("${_id}")' class="fa-solid fa-arrow-right" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
+               </div>
            </div>
         </div>
       </div>
         `
+       
         newsContainer.appendChild(div)
      })
+}
+const loadNewsDetails=(_id)=>{
+ try{
+     fetch(`https://openapi.programming-hero.com/api/news/${_id}`)
+     .then(res=>res.json())
+     .then(data=>showNewsDetails(data.data))
+ }
+ catch(error){
+       console.log(error)
+ }
+}
+const showNewsDetails=(newsDetails)=>{
+        const modalContainer=document.getElementById('modal-container');
+          modalContainer.innerText=''
+        newsDetails.forEach(newsDetail=>{
+          console.log(newsDetail)
+          const {thumbnail_url,details,title,others_info,rating}=newsDetail;
+          const div=document.createElement('div')
+          div.classList.add("modal-content");
+          div.innerHTML=`
+          <div class="modal-header">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">${title}<span class="badge text-bg-warning">${others_info.is_trending? 'Tranding':'No'}</span> </h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <img src="${thumbnail_url}" alt="">
+        <p>${details}</p>
+        </div>
+        <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          
+        </div>
+          `
+          modalContainer.appendChild(div)
+
+        })
+}
+
+const showTrending=()=>{
+   const isTrending=fetchData.filter(singleData=>singleData.others_info.is_trending===true)
+   showNews(isTrending,categoryName)
+   
+}
+const showTodayPic=()=>{
+  const isTodayPic=fetchData.filter(singleData=>singleData.others_info.is_todays_pick===true);
+  showNews(isTodayPic,categoryName)
+}
+// generate rating
+const generatingRating=(rating)=>{
+   let ratingHTML='';
+   for(let i=1;i<=Math.floor(rating);i++){
+    ratingHTML+=`<i class='fas fa-star'></i>`
+   }
+   if(rating-Math.floor(rating)>0){
+    ratingHTML+=`<i class='fas fa-star-half'></i>`
+   }
+   return ratingHTML;
 }
 
 loadCategoryName()
